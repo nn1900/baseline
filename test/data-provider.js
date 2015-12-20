ef0fa5937dca815615d60833f1b884da9fdb0628
eric <eric.blueplus@gmail.com>
2015-12-20 19:21:34 +0800

    Added data provider factory method

---
+++ test/data-provider.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var chai = require('chai');
+var expect = require('chai').expect;
+var path = require('path');
+var getProvider = require('../lib/getProvider');
+
+/* polyfill Promise for older Node.js */
+require('chai').should();
+
+chai.use(require('chai-as-promised'));
+
+describe('data providers', function() {
+
+  describe('default data providers behavior', function() {
+    it('should throws error if getting non-existed data provider', function() {
+      expect(function() { getProvider('not_existed'); })
+        .to.throw(/has not been implemented yet/);
+    });
+
+    it('should successfully get the mysql data provider', function() {
+      getProvider('mysql').should.be.an('object');
+    });
+  });
+});
